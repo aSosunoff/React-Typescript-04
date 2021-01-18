2900d9264944eb7dbcc28e288f521d59656d10aa
asosunoff <al.sosunoff@gmail.com>
2021-01-19 01:41:40 +0300

    fix homa page

--- src/components/pages/home-page.tsx
+++ src/components/pages/home-page.tsx
@@ -1,10 +1,8 @@
 import React from "react";
-import { useBookstoreServiceContext } from "../../context/bookstore-service-context";
 import BookList from "../book-list";
 
 const HomePage: React.FC = () => {
-  const { getBook } = useBookstoreServiceContext();
-  return <BookList books={getBook()} onAddedToCart={() => {}} />;
+  return <BookList onAddedToCart={() => {}} />;
 };
 
 export default HomePage;
