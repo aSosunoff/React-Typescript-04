c9cc516fa7d7dd5fadf1cfa2a7f4fd187c47cf36
asosunoff <al.sosunoff@gmail.com>
2021-01-18 22:40:37 +0300

    added error indicator

---
+++ src/components/error-indicator/ErrorIndicator.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import styles from "./ErrorIndicator.module.scss";
+import icon from "./death-star.png";
+
+const ErrorIndicator: React.FC = () => {
+  return (
+    <div className={styles["error-indicator"]}>
+      <img src={icon} alt="error icon" />
+      <span className={styles["boom"]}>BOOM!</span>
+      <span>something has gone terribly wrong</span>
+      <span>(but we already sent droids to fix it)</span>
+    </div>
+  );
+};
+
+export default ErrorIndicator;
