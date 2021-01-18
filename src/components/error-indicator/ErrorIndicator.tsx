8be5e9af888b67a6f28cc3d057c438f3eb740a38
asosunoff <al.sosunoff@gmail.com>
2021-01-18 23:00:49 +0300

    fixfix

--- src/components/error-indicator/ErrorIndicator.tsx
+++ src/components/error-indicator/ErrorIndicator.tsx
@@ -7,8 +7,6 @@ const ErrorIndicator: React.FC = () => {
     <div className={styles["error-indicator"]}>
       <img src={icon} alt="error icon" />
       <span className={styles["boom"]}>BOOM!</span>
-      <span>something has gone terribly wrong</span>
-      <span>(but we already sent droids to fix it)</span>
     </div>
   );
 };
