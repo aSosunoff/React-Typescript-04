a6baaaf2c164f63154bff0b035f51d4ecdb6b17e
asosunoff <al.sosunoff@gmail.com>
2021-01-19 00:51:55 +0300

    fix structure

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,13 +1,9 @@
 import React from "react";
-import {
-  BookstoreServiceProvider,
-  useBookstoreServiceContext,
-} from "../../context/bookstore-service-context";
-import { BuildHoC } from "../../HOC/build-hoc";
+import { useBookstoreServiceContext } from "../../context/bookstore-service-context";
 
 const App: React.FC = () => {
   const { getBook } = useBookstoreServiceContext();
   return <div>App {getBook()}</div>;
 };
 
-export default BuildHoC(BookstoreServiceProvider, () => null)(App);
+export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
 import App from "./components/app";
+import store from "./store";
+import { BookstoreServiceProvider } from "./context/bookstore-service-context";
+import ErrorBoundaryIndicator from "./components/error-boundary-indicator";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <Provider store={store}>
+    <ErrorBoundaryIndicator>
+      <BookstoreServiceProvider>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </BookstoreServiceProvider>
+    </ErrorBoundaryIndicator>
+  </Provider>,
+  document.getElementById("root")
+);
