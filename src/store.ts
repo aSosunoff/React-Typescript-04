eed5fc16d2b95fe465f1d5b782fc36d6aaf7de14
asosunoff <al.sosunoff@gmail.com>
2021-01-21 21:59:55 +0300

    fix store

--- src/store.ts
+++ src/store.ts
@@ -4,12 +4,10 @@ import { composeWithDevTools } from "redux-devtools-extension";
 
 import reducer from "./redux/reducers";
 import { logMiddleware } from "./redux/middleware/logMiddleware";
-/* import api from "./middleware/api"; */
 
 const enhancer = applyMiddleware(thunk, logMiddleware);
 
 export default createStore(
     reducer,
-    /* persistedState, */
     composeWithDevTools(enhancer)
 );
