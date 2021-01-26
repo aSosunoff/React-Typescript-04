db547ed133ddce11c8adcea97a997df0f34f9de9
asosunoff <al.sosunoff@gmail.com>
2021-01-26 18:41:18 +0300

    fix

--- src/components/pages/home-page.tsx
+++ src/components/pages/home-page.tsx
@@ -6,7 +6,7 @@ const HomePage: React.FC = () => {
   return (
     <div>
       <BookList />
-      <ShoppingCartTable {...({} as any)} />
+      <ShoppingCartTable />
     </div>
   );
 };
--- src/redux/actions/books-action.ts
+++ src/redux/actions/books-action.ts
@@ -1,31 +1,35 @@
 import { IBook } from "../../interfaces/IBook";
-import { Dispatch } from 'redux';
-import { ActionTypeBooksLoad, ActionTypeBooksRequest, ActionTypeBooksError } from "../reducers/booksReducer";
+import { Dispatch } from "redux";
+import {
+  ActionTypeBooksLoad,
+  ActionTypeBooksRequest,
+  ActionTypeBooksError,
+} from "../reducers/booksReducer";
 
 const loadedBooks = (payload: IBook[]): ActionTypeBooksLoad => {
-    return {
-        type: 'BOOKS_SUCCESS',
-        payload
-    }
+  return {
+    type: "BOOKS_SUCCESS",
+    payload,
+  };
 };
 
 const requestBooks = (): ActionTypeBooksRequest => {
-    return {
-        type: 'BOOKS_REQUEST',
-    }
+  return {
+    type: "BOOKS_REQUEST",
+  };
 };
 
 const errorBooks = (payload: string): ActionTypeBooksError => {
-    return {
-        type: 'BOOKS_FAILURE',
-        payload
-    }
+  return {
+    type: "BOOKS_FAILURE",
+    payload,
+  };
 };
 
 export const fetchBooks = (getBook: () => Promise<IBook[]>, dispatch: Dispatch) => () => {
-    dispatch(requestBooks());
+  dispatch(requestBooks());
 
-    getBook()
-        .then((date) => dispatch(loadedBooks(date)))
-        .catch((er) => dispatch(errorBooks(er.message)));
+  getBook()
+    .then((date) => dispatch(loadedBooks(date)))
+    .catch((er) => dispatch(errorBooks(er.message)));
 };
